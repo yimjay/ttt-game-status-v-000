4068cb5d986a929fb7bdd4184c09ed9b9c2dab80
Ji Yim <gsooyim@gmail.com>
2018-04-05 01:28:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,6 +22,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if position_taken?(board, board[win_combination[2]]) && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] ==  board[win_combination[2]]
       return win_combination
+      binding.pry
     end
   end
   false
